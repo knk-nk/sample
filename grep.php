9ab3dc5fdd59878bc9e821fa2ac0cc64fb3318fd
generic_shroomy_cat <124710883+knk-nk@users.noreply.github.com>
2025-02-17 18:26:39 +0400

    Add files via upload

--- grep.php
+++ grep.php
@@ -1,14 +1,15 @@
 <form action="<?=$PHP_SELF;?>" method="POST" style="font-family: Verdana">
 	<br><input type="text" name="grep" placeholder="Поиск текста в коде"><br>
-	<input type="text" name="ext" placeholder="Расширение файла" value=".php"><br><br>
+	<input type="text" name="ext" placeholder="Расширение файла" value=".php" pattern="[.][A-Za-z0-9]*"><br><br>
 	<input type="text" name="find" placeholder="Поиск по названию файла"><br><br>
 	<input type="submit" value="Найти">
 </form><hr>
 
 <?
 if (!empty($_POST)) {
-$str_grep=$_POST["grep"];
-$str_find=$_POST["find"];
+$ptrn=array('\\', '\r', '\n');
+$str_grep=str_replace($ptrn, '', $_POST["grep"]);
+$str_find=str_replace($ptrn, '', $_POST["find"]);
 $ext=$_POST["ext"];
 
 echo '<pre>';
